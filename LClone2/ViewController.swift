a8acd245c5deb1d566799195a43bbdbeb786a36d
John <capraru.ioan@gmail.com>
2022-06-09 14:38:29 +0300

    Initial Commit

---
+++ LClone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LClone2
+//
+//  Created by Ionut Capraru on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
